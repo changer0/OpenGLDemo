46edf37af5028a9afe48684db8668b2ebba79983
zhanglulu <zhangulu1007@foxmail.com>
2018-07-26 14:01:29 +0800

    画了螺旋: 1. 学习点的画法; 2. 学习坐标系的旋转.

--- app/src/main/java/com/example/lulu/opengldemo/MainActivity.java
+++ app/src/main/java/com/example/lulu/opengldemo/MainActivity.java
@@ -23,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         MyGLSurfaceView view = new MyGLSurfaceView(this);
         //renderer: 渲染器
-        view.setRenderer(new MyRenderer());
+        //view.setRenderer(new MyTriangleRenderer());
+        view.setRenderer(new MyPointRenderer());
         setContentView(view);
     }
     class MyGLSurfaceView extends GLSurfaceView {
@@ -37,79 +38,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //自定义渲染器(OpenGL ES 开发中最为重要的部分)
-    class MyRenderer implements GLSurfaceView.Renderer {
-
-        private float ratio;
-
-        //表层创建时
-        @Override
-        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-            //设置清屏色
-            gl.glClearColor(0, 0, 0, 1);
-            //启用顶点缓冲区
-            gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-        }
-
-        //表层size变化时
-        @Override
-        public void onSurfaceChanged(GL10 gl, int width, int height) {
-            //设置视口 输出画面的区域 (从左下角开始)
-            gl.glViewport(0, 0, width, height);
-
-            ratio = (float)width / (float)height;
-            //设置矩阵模式  投影矩阵 (openGL基于状态机)
-            gl.glMatrixMode(GL10.GL_PROJECTION);
-            //加载单位矩阵
-            gl.glLoadIdentity();
-            //设置平截头体 zNear:近平面 zFar:远平面
-            gl.glFrustumf(-1f, 1f, -ratio, ratio, 3, 7);
-        }
-
-        //绘图
-        @Override
-        public void onDrawFrame(GL10 gl) {
-            //清除颜色缓冲区
-            gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-            //模型视图矩阵
-            gl.glMatrixMode(GL10.GL_MODELVIEW);
-            //加载单位矩阵
-            gl.glLoadIdentity();
-            //eyex, eyey, eyez,:放置眼球的坐标
-            //centerx, centery, centerz : 眼球的观察点
-            //upx, upy, upz : 指定眼球向上的向量
-            GLU.gluLookAt(gl, 0, 0, 5, 0, 0, 0, 0, 1, 0);
-
-            //画三角形
-            //绘制数组
-            //三角形坐标
-            float[] coords = {
-                    0f, ratio, 2f,
-                    -1f, -ratio, 2f,
-                    1f, -ratio, 2f,
-            };
-            //分配字节缓存区空间, 存放定点坐标数据
-            // 一个字节 8位  浮点数4个字节
-            ByteBuffer ibb = ByteBuffer.allocateDirect(coords.length * 4);
-            //设置顺序(本地顺序,跟操作系统有关)
-            ibb.order(ByteOrder.nativeOrder());
-            //放置顶点坐标数据
-            FloatBuffer fbb = ibb.asFloatBuffer();
-            fbb.put(coords);
-            //定位指针的位置, 从该位置开始读取顶点数据
-            fbb.position(0);
-            //设置绘图的颜色 (红色)
-            gl.glColor4f(1f, 0f, 0f, 1f);
-
-            //加载顶点指针
-            // size: 使用三个坐标值表示一个点
-            // type: 每个点的数据类型
-            // stride: 0, 跨度
-            // pointer: 指定顶点缓存区
-            gl.glVertexPointer(3, GL10.GL_FLOAT, 0, ibb);
-            //绘制三角形
-            gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
-
-        }
-    }
 }
---
+++ app/src/main/java/com/example/lulu/opengldemo/MyPointRenderer.java
@@ -0,0 +1,91 @@
+package com.example.lulu.opengldemo;
+
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by zhanglulu on 2018/7/26.
+ * for 点渲染器 绘制螺旋线
+ */
+public class MyPointRenderer implements GLSurfaceView.Renderer{
+
+    private float ratio;
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        //设置清屏色
+        gl.glClearColor(0f, 0f, 0f, 1f);
+        //启用顶点缓冲区
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        //设置视口
+        gl.glViewport(0, 0, width, height);
+
+        //设置矩阵模式为投影矩阵, 为设置平截头体做准备
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        //加载单位矩阵
+        gl.glLoadIdentity();
+        ratio = ((float) width) / ((float) height);
+        //设置平截头体
+        gl.glFrustumf(-1f, 1f, -ratio, ratio, 3, 7);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        //清除颜色缓冲区
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+        //设置绘图颜色
+        gl.glColor4f(1f, 0f, 0f, 1f);
+
+        //操作模型视图
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        //设置眼球的位置
+        GLU.gluLookAt(gl, 0f, 0f,5f, 0f, 0f, 0f, 0f, 1f, 0f);
+
+        //旋转角度
+        //angle: 旋转角度
+        //x,y,z: 沿着哪个轴旋转(向量) 迎面轴正方向看去, 顺时针: 负值 逆时针: 正值
+        gl.glRotatef(-90, 1, 0, 0);
+
+        //计算点坐标
+        float r = 0.5f;//半径
+        List<Float> coordsList = new ArrayList<>();//坐标点集合
+        //  旋转3圈
+        float x = 0f, y = 0f, z = 1.5f;
+        float zstep = 0.005f;//步长
+        for (float alpha = 0f; alpha < Math.PI * 6; alpha = (float) (alpha + Math.PI/32)) {
+            x = (float) (r * Math.cos(alpha));
+            y = (float) (r * Math.sin(alpha));
+            z = z - zstep;
+            coordsList.add(x);
+            coordsList.add(y);
+            coordsList.add(z);
+        }
+
+        //转换点成为缓冲区
+        ByteBuffer ibb = ByteBuffer.allocateDirect(coordsList.size() * 4);
+        ibb.order(ByteOrder.nativeOrder());
+        FloatBuffer fbb = ibb.asFloatBuffer();
+        for (Float f : coordsList) {
+            fbb.put(f);
+        }
+        fbb.position(0);
+        //指定顶点指针
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, ibb);
+        //画数组 (0
+        gl.glDrawArrays(GL10.GL_POINTS, 0, coordsList.size() / 3);
+    }
+}
