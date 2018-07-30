fe7332cc28b47af987426e17d6d9a4bb59604c5c
DESKTOP-H58BO6B\lulu <zhanglulu1210@gmail.com>
2018-07-30 22:22:00 +0800

    1. 加上底部控制器

--- app/src/main/java/com/example/lulu/opengldemo/MainActivity.java
+++ app/src/main/java/com/example/lulu/opengldemo/MainActivity.java
@@ -5,17 +5,21 @@ import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AbstractMyRenderer renderer;
     private MyGLSurfaceView myGLSurfaceView;
+    private Button left, right, top, bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         myGLSurfaceView = new MyGLSurfaceView(this);
         //renderer: 渲染器
         renderer = new MyTriangleConeRenderer();
@@ -24,8 +28,21 @@ public class MainActivity extends AppCompatActivity {
         //GLSurfaceView.RENDERMODE_CONTINUOUSLY: 持续渲染(默认)
         //GLSurfaceView.RENDERMODE_WHEN_DIRTY: 脏渲染, 命令渲染
         myGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-        setContentView(myGLSurfaceView);
+        ((ViewGroup) findViewById(R.id.gl_surface_view)).addView(myGLSurfaceView);
+
+        left = ((Button) findViewById(R.id.left));
+        right = ((Button) findViewById(R.id.right));
+        top = ((Button) findViewById(R.id.top));
+        bottom = ((Button) findViewById(R.id.bottom));
+
+        left.setOnClickListener(this);
+        right.setOnClickListener(this);
+        top.setOnClickListener(this);
+        bottom.setOnClickListener(this);
     }
+
+
+
     class MyGLSurfaceView extends GLSurfaceView {
 
         public MyGLSurfaceView(Context context) {
@@ -38,18 +55,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    public void onClick(View v) {
         float step = 5f;
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_BACK:
-                renderer.xrotate  = renderer.xrotate - step;//沿x轴向上旋转
-                break;
-            case KeyEvent.KEYCODE_MENU:
+        switch (v.getId()) {
+            case R.id.left:
                 renderer.yrotate = renderer.yrotate + step;//沿y轴向左旋转
+                break;
+            case R.id.right:
+                renderer.yrotate = renderer.yrotate - step;//沿y轴向右旋转
+                break;
+            case R.id.top:
+                renderer.xrotate = renderer.xrotate - step;//沿x轴向上旋转
+                break;
+            case R.id.bottom:
+                renderer.xrotate = renderer.xrotate + step;//沿x轴向下旋转
                 break;
         }
         //请求渲染, 与脏渲染配合使用
         myGLSurfaceView.requestRender();
-        return true;
     }
 }
